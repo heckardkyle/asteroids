103c15c6f57e8ce0096b12fe829225c7ba6223ec
heckardkyle <heckardkyle@yahoo.com>
2025-02-01 22:26:55 -0800

    create game clock and render player on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +9,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0)) # black screen
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
 
 if __name__ == "__main__":
     main()
